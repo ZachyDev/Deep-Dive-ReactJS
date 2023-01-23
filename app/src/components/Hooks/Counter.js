cc7c9beb8db85242dfc30a71128e7c517eadc801
ZachyDev <mosetizachary001@gmail.com>
2023-01-23 20:40:12 +0300

    Reviewing rules of creating hooks/custom hooks

--- app/src/components/Hooks/Counter.js
+++ app/src/components/Hooks/Counter.js
@@ -4,8 +4,10 @@ function Counter() {
     const [counter,setCounter] = useState(0);
     
     useEffect(() => {
+       if(counter === 10) {
         document.body.style.backgroundImage = 'linear-gradient(to right, coral, teal)';
-    },[])
+       }
+    },[counter])
     // update counters
     const updateCounter = () => {
         setCounter(counter + 1);
