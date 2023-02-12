e8c7e31cda5fca58abe592f9a8200f59c26db1ea
ZachyDev <mosetizachary001@gmail.com>
2023-02-12 22:25:27 +0300

    how an image gallery uses and updates state on click:

--- app/src/components/SculptureInfo.js
+++ app/src/components/SculptureInfo.js
@@ -1,18 +1,34 @@
-import React,{useContext} from 'react'
+import React,{useContext, useState} from 'react'
 import { DataContext } from '../contexts/Data'
 
 function SculptureInfo() {
   const {sculptureList} = useContext(DataContext)
 
+  const [index,setIndex] = useState(0);
+  const [showMore,setShowMore] = useState(false);
+
+  const handleNextClick = () => {
+    setIndex(index + 1);
+  }
+  const handleMoreClick = () => {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+
   return (
     <div>
-        {sculptureList.map(sculpture => {
-            return(
-                <div key={sculpture.name}>
-                    <span>{sculpture.name}</span>
-                </div>
-            )
-        })}
+      <button onClick={handleNextClick}>Next</button>
+       <h2>{sculpture.name} by {sculpture.artist}</h2>
+       <h3>
+        ({index+1} of {sculptureList.length})
+       </h3>
+       <button onClick={handleMoreClick}>{showMore ? 'Hide' : 'Show'} details</button>
+       {showMore && <p>{sculpture.description}</p>}
+       <img
+       src={sculpture.url}
+       alt={sculpture.alt} 
+       />
     </div>
   )
 }
